fe69abb8b66fd0c666700478bfe6853e3f4edec4
Rob Kokochak <robkokochak@gmail.com>
2023-11-25 21:55:06 -0500

    prisma db functionality in place

--- components/OverviewContent.tsx
+++ components/OverviewContent.tsx
@@ -1,11 +1,21 @@
-export default function OverviewContent() {
+import prisma from "@/db"
+
+export default async function OverviewContent() {
+  await prisma.conference.create({ data: { title: "Information Systems Conference", deadline: new Date('25 December 2023').toISOString()}});
+  const conferences = await prisma.conference.findMany();
+
   return (
-    <div>
+    <>
       <div id="overview-content" className="flex py-10">
         <div id="upcoming-conf-card" className="flex-1 p-5 mr-5 max-w-sm rounded-xl bg-dark-color">
           <h4 className="mb-20 text-lg text-white">
             Upcoming Conferences
           </h4>
+          <ul>
+            {conferences.map(conference => (
+              <li key={conference.id}>{conference.title}</li>
+            ))}
+          </ul>
         </div>
         <div id="content-right-col" className="flex-1">
           <div id="submitted-papers-card" className="p-5 mb-3 rounded-xl bg-dark-color">
@@ -20,6 +30,6 @@ export default function OverviewContent() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
